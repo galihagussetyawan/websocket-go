bfcbbf9f9ba4463477a96a55347e08500d4bec3a
galihagussetyawan <galihdevelopment@gmail.com>
2023-02-04 10:45:13 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	log.Println("server running http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
